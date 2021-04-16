de4d6502f1d0daeb6d65475dbe35f2df74dfef00
ptung <misuke01@gmail.com>
2021-04-16 17:21:38 -0400

    Initial commit

---
+++ src/main/java/com/ktung/happygiphy/HappyGiphyApplication.java
@@ -0,0 +1,13 @@
+package com.ktung.happygiphy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyGiphyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyGiphyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ktung/happygiphy/HappyGiphyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktung.happygiphy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyGiphyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
